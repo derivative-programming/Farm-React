523b110399070053995c58ce77a8fb957ec04d1d
petemihaylov <peps.mihailov@gmail.com>
2023-04-23 18:11:56 +0200

    Intercept 401 unauth api errors and redirect to login page

--- src/apiConfig/index.js
+++ src/apiConfig/index.js
@@ -1,27 +1,25 @@
 import axios from "axios";
- 
- 
+import { useNavigate } from "react-router-dom";
+
 export const apiInstance = axios.create({
-  baseURL:
-    "https://dp-farm-pageapi.azurewebsites.net/api/v1_0",
+  baseURL: "https://dp-farm-pageapi.azurewebsites.net/api/v1_0",
 });
 
+// Add a request interceptor
 apiInstance.interceptors.request.use(
-  function (config) {
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
+  (config) => config,
+  (error) => Promise.reject(error)
 );
 
 // Add a response interceptor
 apiInstance.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
+  (response) => response,
+  (error) => {
+    const navigate = useNavigate();
+    if (error.response.status === 401) {
+      // Redirect to login page
+      navigate("/tac-login");
+    }
     return Promise.reject(error);
   }
 );
-
