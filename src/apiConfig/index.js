59caef3a95d3d8cbc8c11301a1db8272f808aaf9
JLemuel <79501195+JLemuel@users.noreply.github.com>
2023-04-24 09:14:35 +0800

    intercept 401 errors and redirects them to login page

--- src/apiConfig/index.js
+++ src/apiConfig/index.js
@@ -1,9 +1,37 @@
+// old code
+
+// import axios from "axios";
+ 
+ 
+// export const apiInstance = axios.create({
+//   baseURL:
+//     "https://dp-farm-pageapi.azurewebsites.net/api/v1_0",
+// });
+
+// apiInstance.interceptors.request.use(
+//   function (config) {
+//     return config;
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   }
+// );
+
+// // Add a response interceptor
+// apiInstance.interceptors.response.use(
+//   function (response) {
+//     return response;
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   }
+// );
+
+// this my new code
 import axios from "axios";
- 
- 
+
 export const apiInstance = axios.create({
-  baseURL:
-    "https://dp-farm-pageapi.azurewebsites.net/api/v1_0",
+  baseURL: "https://dp-farm-pageapi.azurewebsites.net/api/v1_0",
 });
 
 apiInstance.interceptors.request.use(
@@ -21,7 +49,12 @@ apiInstance.interceptors.response.use(
     return response;
   },
   function (error) {
+    // Check if the error is a 401 Unauthorized error
+    if (error.response && error.response.status === 401) {
+      // Redirect to the login page if a 401 error is encountered
+      window.location.href = '/tac-login';
+    }
+
     return Promise.reject(error);
   }
 );
-
