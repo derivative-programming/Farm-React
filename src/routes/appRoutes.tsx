56d1102060a38b49ef197ca4f763d27c359f3308
Vince Roche <vince.roche@gmail.com>
2023-04-25 05:18:56 -0400

    lazy loading in router

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,21 +1,30 @@
-import React, { useContext } from "react";
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LayoutComponent from "../components/Layout/Layout"; 
-import * as ReportConnected from "../components/reports/connected";
-import * as FormConnected from "../components/forms/connected"; 
+// import * as ReportConnected from "../components/reports/connected";
+// import * as FormConnected from "../components/forms/connected"; 
+import loadable from "@loadable/component";
+
+const ReportConnectedTacFarmDashboard = loadable(() => import('../components/reports/connected/TacFarmDashboard'));
+const ReportConnectedLandPlantList = loadable(() => import('../components/reports/connected/LandPlantList'));
+const ReportConnectedPlantUserDetails = loadable(() => import('../components/reports/connected/PlantUserDetails'))
+const FormConnectedLandAddPlant = loadable(() => import('../components/forms/connected/LandAddPlant'));
+const FormConnectedPlantEdit = loadable(() => import('../components/forms/connected/PlantEdit'));
+const FormConnectedTacLogin = loadable(() => import('../components/forms/connected/TacLogin'));
+const FormConnectedTacRegister = loadable(() => import('../components/forms/connected/TacRegister'));
+const Logout = loadable(() => import('../components/forms/connected/logout'));
 
 const AppRoute = () => {
   return (
     <LayoutComponent>
     <Routes>  
-      <Route path={"/tac-farm-dashboard/:id"} element={<ReportConnected.ReportConnectedTacFarmDashboard />} />
-      <Route path={"/land-plant-list/:id"} element={<ReportConnected.ReportConnectedLandPlantList />} />
-      <Route path={"/plant-user-details/:id"} element={<ReportConnected.ReportConnectedPlantUserDetails />} />
-      <Route path={"/land-add-plant/:id"} element={<FormConnected.FormConnectedLandAddPlant />} />
-      <Route path={"/plant-edit/:id"} element={<FormConnected.FormConnectedPlantEdit />} />
-      <Route path={"/tac-login/:id"} element={<FormConnected.FormConnectedTacLogin />} />
-      <Route path={"/tac-register/:id"} element={<FormConnected.FormConnectedTacRegister />} />
-      <Route path={"/logout"} element={< FormConnected.Logout />} />
+      <Route path={"/tac-farm-dashboard/:id"} element={<ReportConnectedTacFarmDashboard />} />
+      <Route path={"/land-plant-list/:id"} element={<ReportConnectedLandPlantList />} />
+      <Route path={"/plant-user-details/:id"} element={<ReportConnectedPlantUserDetails />} />
+      <Route path={"/land-add-plant/:id"} element={<FormConnectedLandAddPlant />} />
+      <Route path={"/plant-edit/:id"} element={<FormConnectedPlantEdit />} />
+      <Route path={"/tac-login/:id"} element={<FormConnectedTacLogin />} />
+      <Route path={"/tac-register/:id"} element={<FormConnectedTacRegister />} />
+      <Route path={"/logout"} element={< Logout />} />
       <Route path={"*"} element={<Navigate to={"/tac-farm-dashboard/00000000-0000-0000-0000-000000000000"} replace />} />
     </Routes>
     </LayoutComponent>
