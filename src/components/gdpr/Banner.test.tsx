5d49d5815aa5a89ffa1b0cd0aae3be430d2739f3
Inam-urRehman <mindgripper.developement@gmail.com>
2023-04-29 22:29:00 +0500

    Add test cases for GDPRBanner

---
+++ src/components/gdpr/Banner.test.tsx
@@ -0,0 +1,37 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import GDPRBanner from "./Banner";
+
+describe("GDPRBanner", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it("should show the banner initially", () => {
+    render(<GDPRBanner />);
+
+    expect(screen.getByText(/This website uses cookies/)).toBeInTheDocument();
+  });
+
+  it("should hide the banner after accepting", () => {
+    render(<GDPRBanner />);
+
+    const acceptButton = screen.getByRole("button", { name: "Accept" });
+
+    fireEvent.click(acceptButton);
+
+    expect(localStorage.getItem("gdpr_accepted")).toBe("true");
+    expect(
+      screen.queryByText(/This website uses cookies/)
+    ).not.toBeInTheDocument();
+  });
+
+  it("should hide the banner if already accepted", () => {
+    localStorage.setItem("gdpr_accepted", "true");
+
+    render(<GDPRBanner />);
+
+    expect(
+      screen.queryByText(/This website uses cookies/)
+    ).not.toBeInTheDocument();
+  });
+});
