9365d90392a7de5622cf69cf0c951b992c45aa5e
Yurec <yurec133@gmail.com>
2023-05-01 16:15:07 +0300

    feat(indexedDB): add a record to the database

---
+++ src/hooks/useNavigatorOnLine.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function useNavigatorOnline() {
+  const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
+
+  useEffect(() => {
+    function handleOnlineStatus() {
+      setIsOnline(window.navigator.onLine);
+    }
+
+    window.addEventListener("online", handleOnlineStatus);
+    window.addEventListener("offline", handleOnlineStatus);
+
+    return () => {
+      window.removeEventListener("online", handleOnlineStatus);
+      window.removeEventListener("offline", handleOnlineStatus);
+    };
+  }, []);
+
+  return isOnline;
+}
+export default useNavigatorOnline
