f962153784e7ec3ef5c15b0bb3776731e8f76330
Vince Roche <vince.roche@gmail.com>
2024-09-22 07:42:43 -0400

    date and datetime headers

--- src/components/forms/headers/LandAddPlantInitObjWF.tsx
+++ src/components/forms/headers/LandAddPlantInitObjWF.tsx
@@ -20,6 +20,8 @@ const HeaderLandAddPlant: FC<HeaderLandAddPlantProps> = ({
 }): ReactElement => {
 
   const landNameHeaderIsVisible = true; 
+  const currentDateHeaderValHeaderIsVisible = true; 
+  const currentDateTimeHeaderValHeaderIsVisible = true; 
 
 
   return (
@@ -31,12 +33,12 @@ const HeaderLandAddPlant: FC<HeaderLandAddPlantProps> = ({
         <dd className="col-sm-9" hidden={!landNameHeaderIsVisible}>{Parser(initData.landName)}</dd>
       </>
       <> {/*currentDateHeaderVal*/}
-        <dt className="col-sm-3" hidden={!landNameHeaderIsVisible}>Current Date</dt>
-        <dd className="col-sm-9" hidden={!landNameHeaderIsVisible}>{formatDate(initData.currentDateHeaderVal)}</dd>
+        <dt className="col-sm-3" hidden={!currentDateHeaderValHeaderIsVisible}>Current Date</dt>
+        <dd className="col-sm-9" hidden={!currentDateHeaderValHeaderIsVisible}>{formatDate(initData.currentDateHeaderVal)}</dd>
       </>
       <> {/*currentDateTimeHeaderVal*/}
-        <dt className="col-sm-3" hidden={!landNameHeaderIsVisible}>Current Date Time</dt>
-        <dd className="col-sm-9" hidden={!landNameHeaderIsVisible}>{formatDateTime(initData.currentDateTimeHeaderVal)}</dd>
+        <dt className="col-sm-3" hidden={!currentDateTimeHeaderValHeaderIsVisible}>Current Date Time</dt>
+        <dd className="col-sm-9" hidden={!currentDateTimeHeaderValHeaderIsVisible}>{formatDateTime(initData.currentDateTimeHeaderVal)}</dd>
       </>
     </dl>
   );
--- src/components/reports/headers/LandPlantListInitReport.tsx
+++ src/components/reports/headers/LandPlantListInitReport.tsx
@@ -5,6 +5,7 @@ import React, {
 } from "react";
 import * as InitReportService from "../services/init/LandPlantListInitReport";
 import Parser from 'html-react-parser'; 
+import {formatDate, formatDateTime} from "../../../common/utilities";
  
 export interface HeaderLandPlantListProps {
   name: string;
@@ -19,6 +20,8 @@ const HeaderLandPlantList: FC<HeaderLandPlantListProps> = ({
 }): ReactElement => {
 
   const landNameHeaderIsVisible = true; 
+  const currentDateHeaderValHeaderIsVisible = true; 
+  const currentDateTimeHeaderValHeaderIsVisible = true; 
 
   return ( 
     <div className="ms-3">
@@ -29,6 +32,14 @@ const HeaderLandPlantList: FC<HeaderLandPlantListProps> = ({
           <dt className="col-sm-3" hidden={!landNameHeaderIsVisible}>Land Name</dt>
           <dd className="col-sm-9" hidden={!landNameHeaderIsVisible}>{Parser(initData.landName)}</dd>
         </> 
+      <> {/*currentDateHeaderVal*/}
+        <dt className="col-sm-3" hidden={!currentDateHeaderValHeaderIsVisible}>Current Date</dt>
+        <dd className="col-sm-9" hidden={!currentDateHeaderValHeaderIsVisible}>{formatDate(initData.currentDateHeaderVal)}</dd>
+      </>
+      <> {/*currentDateTimeHeaderVal*/}
+        <dt className="col-sm-3" hidden={!currentDateTimeHeaderValHeaderIsVisible}>Current Date Time</dt>
+        <dd className="col-sm-9" hidden={!currentDateTimeHeaderValHeaderIsVisible}>{formatDateTime(initData.currentDateTimeHeaderVal)}</dd>
+      </>
       </dl> 
     </div>
   );
